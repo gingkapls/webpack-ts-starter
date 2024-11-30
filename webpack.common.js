ff2cd41c33826682e978316029f44b942c53afcc
gin <73906888+gingkapls@users.noreply.github.com>
2024-11-30 18:38:03 +0530

    webpack: add typescript support

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  context: __dirname, // to automatically find tsconfig.json
   entry: {
-    index: "./src/index.js",
+    index: './src/index.ts',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
-      template: "./src/index.html",
+      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.([cm]?ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
-  optimization: {
-    runtimeChunk: "single",
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,31 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
+  plugins: [
+    // run type-checking in a separate process during builds
+    new ForkTsCheckerWebpackPlugin(),
+    new ForkTsCheckerNotifierWebpackPlugin({
+      title: 'TypeScript',
+      excludeWarnings: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 });

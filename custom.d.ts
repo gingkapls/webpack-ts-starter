5002f99bf417c844300fd49bb2f6fd17df9f1cc5
gin <73906888+gingkapls@users.noreply.github.com>
2024-11-30 18:38:52 +0530

    typescript: add custom image module type declarations

---
+++ custom.d.ts
@@ -0,0 +1,20 @@
+// Required for importing assets
+declare module "*.png" {
+  const content: "*.png";
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: "*.jpeg";
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: "*.jpg";
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: "*.svg";
+  export default content;
+}
